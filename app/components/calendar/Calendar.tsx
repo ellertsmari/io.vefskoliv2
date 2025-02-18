1065da0b41919c267c919d68d6264ef1906b38cb
Fannar <fannarjack@gmail.com>
2025-02-18 11:43:17 +0000

    first cal

---
+++ app/components/calendar/Calendar.tsx
@@ -0,0 +1,7 @@
+import Calendar from "node_modules/@toast-ui/calendar/types";
+import CalContainer from "./style";
+const Cal = () => {
+  return <CalContainer id="calendar" style="height: 600px;"></CalContainer>;
+};
+
+export default Cal;
