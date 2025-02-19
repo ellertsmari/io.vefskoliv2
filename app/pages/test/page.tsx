714695afd85d20b8a5a17e922835d4546538abbe
Alice <alicevisintin@outlook.com>
2025-02-19 10:45:28 +0000

    components folder

---
+++ app/pages/test/page.tsx
@@ -0,0 +1,11 @@
+'use client' 
+import Groups from "app/Components/Groups";
+
+const Test = () => {
+  return ( <div>
+      <h1>test</h1>
+      <Groups></Groups>
+  </div> );
+}
+
+export default Test;
