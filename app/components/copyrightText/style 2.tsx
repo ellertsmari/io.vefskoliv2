992e5fac2c6a4e026a9e05b149b83d4daf2af1f0
Fannar <fannarjack@gmail.com>
2025-02-19 14:02:08 +0000

    resolving conflicts

---
+++ app/components/copyrightText/style 2.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CopyrightContainer = styled.div`
+    position: absolute;
+  bottom: 5px;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  font-size: 20px;
+  color: var(--Dark-blue, #2b5b76);
+  bottom: 2px;
+`;
