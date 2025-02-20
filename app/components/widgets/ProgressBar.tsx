efe398bc421105a483cfcd38726bbc2b0340f19e
Margaridamx <margaridamx@gmail.com>
2025-02-20 14:50:18 +0000

    ProgressBar

---
+++ app/components/widgets/ProgressBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ProgressContainer, LabelContainer, OvalsContainer, Oval } from "./style";
+
+interface ProgressBarProps {
+  label?: string;
+  progress?: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ label, progress = 0 }) => {
+  // Progress between 0 and 100
+  const totalOvals = 20;
+  const filledOvals = Math.round((progress / 100) * totalOvals);
+
+  return (
+    <ProgressContainer>
+      <LabelContainer>
+        <span>{label}</span>
+      </LabelContainer>
+      <OvalsContainer>
+        {Array.from({ length: totalOvals }).map((_, index) => (
+          <Oval key={index} filled={index < filledOvals} />
+        ))}
+      </OvalsContainer>
+    </ProgressContainer>
+  );
+};
+
+export default ProgressBar;
--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,13 +1,42 @@
-"use client"
+"use client";
 
+import ProgressBar from "components/widgets/ProgressBar";
 import TodoList from "../components/todoList/todo";
+import { BoxContainer, ProgressContainer, ProgressWrapper, Progress, Title, Percentage } from "app/components/widgets/style";
 
-const homePage = () => {
+const HomePage = () => {
   return (
     <div>
-     <TodoList/>
+      <TodoList />
+
+      {/* Title outside BoxContainer */}
+      <Title>School Progress</Title>
+
+      <BoxContainer>
+        <ProgressContainer>
+
+          {/* Design Progress */}
+          <ProgressWrapper>
+            <span>Design progress</span> {/* Graph title */}
+            <Progress>
+              <ProgressBar progress={55} />
+              <Percentage>{55}%</Percentage> {/* the percentage appears after the progress bar */}
+            </Progress>
+          </ProgressWrapper>
+
+          {/* Code Progress */}
+          <ProgressWrapper>
+            <span>Code progress</span> {/* Graph title */}
+            <Progress>
+              <ProgressBar progress={35} />
+              <Percentage>{35}%</Percentage> {/* the percentage appears after the progress bar */}
+            </Progress>
+          </ProgressWrapper>
+
+        </ProgressContainer>
+      </BoxContainer>
     </div>
   );
-}
+};
 
-export default homePage;
+export default HomePage;
