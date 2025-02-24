c109fb46866720b737360e94912bec683a1d0129
Margaridamx <margaridamx@gmail.com>
2025-02-24 16:09:28 +0000

    First commit

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import TodoList from "../components/todoList/todo";
+import ToolsAndWebsites from "../components/widgets/Tools";
 
 const homePage = () => {
   return (
     <div>
      <TodoList/>
+     <ToolsAndWebsites />
     </div>
   );
 }
