c1f0c93fbbafebb14d026cbe76e57062820e4bfb
Angelika Ciuchcińska <angelikaciuchcinska@Angelikas-MacBook-Air.local>
2025-02-25 13:47:38 +0000

    created daily plan widget

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import TodoList from "../components/todoList/todo";
+import DailyPlan from "../components/dailyPlan/DailyPlan"
 
 const homePage = () => {
   return (
     <div>
+      <DailyPlan/>
      <TodoList/>
     </div>
   );
