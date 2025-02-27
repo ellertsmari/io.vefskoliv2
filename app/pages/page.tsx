3906660544b6fbacca017628834882d93ec21dab
Heiddis Birta <156322797+HeiddisBirta@users.noreply.github.com>
2025-02-27 20:19:01 +0000

    saving

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,19 +1,19 @@
-'use client'
-
+'use server'
+import GuidesWidget from "./guides-test/page";
 import frame1 from "../../public/Frame1.svg"
 import Image from "next/image";
 
 const landingpage = () => {
-  return (  
-    
-     
+  return (
+
+
     <div style={{ gridArea: "main", backgroundColor: "white", minHeight: "100vh" }}>
       <Image alt="background" style={{width: "100%", height: "100%", objectFit: "cover"}} src={frame1}></Image>
-      </div>
-      
+      <GuidesWidget></GuidesWidget>
+    </div>
+
  );
 
 }
 
 export default landingpage;
-
--- app/style.tsx
+++ app/style.tsx
@@ -3,20 +3,20 @@ import styled from "styled-components";
 
 export const PageWrapper = styled.div`
   display: grid;
-  grid-template-areas: 
+  grid-template-areas:
   "navbar navbar"
-  "sidebar main" 
+  "sidebar main"
   ;
   width: 100%;
   height: 100dvh;
   overflow-x: hidden;
- 
+
 `;
 
 export const NavWrapper = styled.div`
- 
+
   width: 100%;
   height: 100vh;
   overflow-x: hidden;
-  
+
 `;
