46af40eca049a7c2cf1af6cc183ff92d708f4696
Angelika Ciuchcińska <angelikaciuchcinska@Angelikas-MacBook-Air.local>
2025-02-28 11:06:35 +0000

    fixed calendar widget

--- app/components/widgets/widgetCalendarStyle.tsx
+++ app/components/widgets/widgetCalendarStyle.tsx
@@ -7,32 +7,32 @@ import Calendar from "react-calendar";
 export const CalendarComponent = styled(Calendar)`
   /* Ensure the whole calendar has a white background */
   background-color: white;
-  border: 1px solid #E8F1FC;
+  border: 1px solid #e8f1fc;
   border-radius: 8px;
   padding: 20px;
   width: 307px;
-  height: 220px;
+  height: 240px;
   display: flex;
   flex-direction: column;
+  margin-bottom: 14px;
 
-.react-calendar__month-view__days,
-.react-calendar__tile,
-.react-calendar__month-view__days__day,
-.react-calendar__month-view__days__day--neighboringMonth,
-abbr,
-button
-{
-  border: none;
-  font-size: 12px;
-  margin: 0px auto;
-  padding:0px;
-}
+  .react-calendar__month-view__days,
+  .react-calendar__tile,
+  .react-calendar__month-view__days__day,
+  .react-calendar__month-view__days__day--neighboringMonth,
+  abbr,
+  button {
+    border: none;
+    font-size: 12px;
+    margin: 0px auto;
+    padding: 0px;
+  }
 
-.react-calendar__month-view__weekdays__weekday,
-.react-calendar__month-view__weekdays__weekday--weekend{
-  text-align: center;
-}
- 
+  .react-calendar__month-view__weekdays__weekday,
+  .react-calendar__month-view__weekdays__weekday--weekend {
+    text-align: center;
+  }
+
   /* Style the navigation buttons (arrows) */
   .react-calendar__navigation__prev2-button,
   .react-calendar__navigation__next2-button,
@@ -51,8 +51,8 @@ button
   }
 
   .react-calendar__navigation__arrow {
-      color: var(--main-Color);
-      font-size: 25px;
+    color: var(--main-Color);
+    font-size: 25px;
   }
 
   /* Hide the second set of arrows (prev2 and next2) */
@@ -65,7 +65,7 @@ button
   .react-calendar__navigation__label {
     font-size: 20px;
     font-weight: bold;
-    color: #2B5B76; /* Color for the month title */
+    color: #2b5b76; /* Color for the month title */
     text-transform: capitalize;
     display: flex;
     align-items: center;
@@ -86,15 +86,25 @@ button
 
   /* Color for the days of the neighboring months */
   .react-calendar__month-view__days__day--neighboringMonth {
-    color: #B3B3B3; /* Color for neighboring month days (January and March) */
+    color: #b3b3b3; /* Color for neighboring month days (January and March) */
     font-size: 11px;
+  }
+
+  .react-calendar__month-view__days__day.react-calendar__tile--active {
+    background-color: var(--main-Lightblue);
+    color: #2b5b76;
+  }
+
+  .react-calendar__month-view__days__day.react-calendar__tile--active {
+    border: 4px solid var(--main-Lightblue);
+    border-radius: 8px;
   }
 
   /* Styling the weekday names */
   .react-calendar__month-view__weekdays__weekday {
     font-size: 11px; /* Set day of week size to 11px */
     font-weight: bold;
-    color: #2B5B76; /* Color for the weekdays */
+    color: #2b5b76; /* Color for the weekdays */
   }
 
   /* Styling the individual day tiles */
@@ -128,10 +138,21 @@ export const ContainerCalendar = styled.div`
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
 `;
 
-// export const Header=styled.div``
+export const Header = styled.div``;
 
-// export const Month=styled.div``
-// export const WeekDays=styled.div``
+export const Month = styled.div``;
+export const WeekDays = styled.div``;
 
-// export const Days=styled.div``
-// export const MainContainer=styled.div
+export const Days = styled.div``;
+export const Day = styled.div;
+
+export const ContainerMain = styled.div`
+  gap: 21px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  font-size: 16px;
+  color: var(--main-Color);
+`;
