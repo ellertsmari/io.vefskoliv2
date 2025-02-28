94c27d557bcf846ce7551209ad72ebe60ee1891d
crds83 <119341646+crds83@users.noreply.github.com>
2025-02-28 14:55:12 +0000

    colorise button added

--- app/components/sidebar/style.tsx
+++ app/components/sidebar/style.tsx
@@ -1,23 +1,21 @@
 import styled from "styled-components";
 import Image from "next/image";
-import home from "public/Vector.svg"
+import home from "public/Vector.svg";
 
-export const ImageIcon = styled(Image) `
-    transform: translate(15%);
-    height: 60px;
-    width: 60px;
-    border-radius: 8px;
-`
+export const ImageIcon = styled(Image)`
+  transform: translate(15%);
+  height: 60px;
+  width: 60px;
+  border-radius: 8px;
+`;
 
 export const ArrowIcon = styled(ImageIcon)`
-    position: absolute;
-    top: 45px;
-    left: 120px;
-    cursor: pointer;
+  position: absolute;
+  top: 45px;
+  left: 120px;
+  cursor: pointer;
 
-
-    /*transform: translate(220%, 15%);*/
-    height: 44px;
-    width: 44px;
-    
-`
+  /*transform: translate(220%, 15%);*/
+  height: 44px;
+  width: 44px;
+`;
