5282cdee6c972ce84b02d18cdd9f11fb0dfd855d
Jakub Mierzejek <gemixe1234@gmail.com>
2025-09-08 14:39:23 +0000

    made a message on landing page about working in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+"use client"
+
+import { Button } from "globalStyles/buttons/default/style"
+import {Container, StyledImg, Wrapper} from "./style"
+import { Heading2, Paragraph } from "globalStyles/text"
+import Link from "next/link"
+
 const HomePage = () => {
   return (
-    <div>yo</div>
+    <Wrapper>
+      <StyledImg src="https://icons.veryicon.com/png/o/transport/traffic-icon/construction-5.png" alt="construction"/>
+    <Container>
+      <Heading2>The io.vefskoli.is is under construction...</Heading2>
+      <Paragraph>Click the "Log in" button if you want to log in to LMS and resturn your projects</Paragraph>
+      <a href="/signin">
+      <Button style={{width:"128px"}}$styletype="default">LOG IN</Button>
+      </a>
+    </Container>
+    </Wrapper>
+    
   )
 }
 
