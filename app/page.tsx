94b5986e89e3cf6163f2989db4e30fc88edb0289
Jakub Mierzejek <gemixe1234@gmail.com>
2025-09-08 14:54:22 +0000

    fixed a bug

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { Button } from "globalStyles/buttons/default/style"
 import {Container, StyledImg, Wrapper} from "./style"
 import { Heading2, Paragraph } from "globalStyles/text"
-import Link from "next/link"
 
 const HomePage = () => {
   return (
@@ -11,7 +10,7 @@ const HomePage = () => {
       <StyledImg src="https://icons.veryicon.com/png/o/transport/traffic-icon/construction-5.png" alt="construction"/>
     <Container>
       <Heading2>The io.vefskoli.is is under construction...</Heading2>
-      <Paragraph>Click the "Log in" button if you want to log in to LMS and resturn your projects</Paragraph>
+      <Paragraph>Click the Log in button if you want to log in to LMS and resturn your projects</Paragraph>
       <a href="/signin">
       <Button style={{width:"128px"}}$styletype="default">LOG IN</Button>
       </a>
